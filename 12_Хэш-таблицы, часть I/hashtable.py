6b8a2de2d934b1c3d464756155e8d55c3f46f123
Nikita Zaystev <nikitos945max@gmail.com>
2023-01-05 00:33:58 +0300

    12_Хэш-таблицы, часть I

---
+++ 12_Хэш-таблицы, часть I/hashtable.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, key: str, value: object = None) -> None:
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+class HashTable:
+    def __init__(self, size: int) -> None:
+        self.data = [None] * size
+        self.size = size
+
+    @staticmethod
+    def hash_function(key: str, size: int) -> int:
+        hash_key = ''
+        for k in key:
+            hash_key += str(ord(k))
+
+        return int(hash_key) % size
+
+    def add(self, key: str, value: object) -> None:
+        hash_value = self.hash_function(key, self.size)
+        if self.data[hash_value] is None:
+            self.data[hash_value] = Node(key, value)
+        else:
+            temp = Node(key)
+            p = self.data[hash_value]
+            while p.next is not None:
+                p = p.next
+            p.next = temp
+
+    def get(self, key: str) -> object:
+        hash_value = self.hash_function(key, self.size)
+        if self.data[hash_value] is not None and self.data[hash_value].key == key:
+            return self.data[hash_value].value
+        else:
+            p = self.data[hash_value]
+            while p is not None and p.key != key:
+                p = p.next
+            if p is not None and p.key == key:
+                return p.value
+        return False
+
+    def delete(self, key: str) -> None | str:
+        if not self.get(key):
+            return 'Нет такого объекта'
+        hash_value = self.hash_function(key, self.size)
+
+        if self.data[hash_value] is not None and self.data[hash_value].key == key:
+            self.data[hash_value] = None
+        else:
+            p = self.data[hash_value]
+            pre = None
+            while p is not None and p.data != key:
+                pre = p
+                p = p.next
+            if p is None:
+                return 'Delete Error'
+            else:
+                pre.next = p.next
+
+
+if __name__ == '__main__':
+    ht = HashTable(10)
+    ht.add('odin', 1)
+    print(ht.get('odin'))
+    ht.add('dva', 2)
+    print(ht.get('dva'))
+    ht.delete('dva')
+    print(ht.get('dva'))
